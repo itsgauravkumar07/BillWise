b1ebb51233175a2572a17ffe8e44c39a4a858627
Gaurav kumar <01itsgauravpal@gmail.com>
2025-09-06 00:28:13 +0530

    feat: add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { Route, Router, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Setting from "./pages/Setting";
 import AddSubscription from "./pages/AddSubscription";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App(){
 
     return(
         <div>
-            <Navbar />
+            
             <BrowserRouter>
+
+                <Navbar />
+
                 <Routes>
                     <Route path="/" element={ <Dashboard /> } />
                     <Route path="/add" element={ <AddSubscription /> } />
                     <Route path="/setting" element={ <Setting /> } />
                 </Routes>
+
+                <Footer />
             </BrowserRouter>
             
         </div>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+
+    return(
+       <footer className="fixed bottom-0 w-full bg-gray-200 text-white p-4 text-center">
+          <h1 className="text-black">Made with 💖 in 2025</h1>  
+       </footer>
+    )
+}
+  
+export default Footer;
