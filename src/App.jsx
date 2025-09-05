932f5a4d9a2a4eb27a8ef4ed2f6d6dcf6e551ca5
Gaurav kumar <01itsgauravpal@gmail.com>
2025-09-05 15:59:04 +0530

    feat: add Navbar component with navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Route, Router, BrowserRouter, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Setting from "./pages/Setting";
 import AddSubscription from "./pages/AddSubscription";
+import Navbar from "./components/Navbar";
 
 function App(){
 
     return(
         <div>
+            <Navbar />
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={ <Dashboard /> } />
-                    <Route path="/addsubscription" element={ <AddSubscription /> } />
+                    <Route path="/add" element={ <AddSubscription /> } />
                     <Route path="/setting" element={ <Setting /> } />
                 </Routes>
             </BrowserRouter>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+
+    return(
+        <div>
+            <a href="/">Dashboard</a>
+            <a href="/add">Add Subscription</a>
+            <a href="/setting">Setting</a>
+        </div>
+    )
+}
+
+export default Navbar;
